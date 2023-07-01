f5121fc606fe6331f2ea86c3702d609bbb4140ae
nadeesha_dileen <nadeeshadilee@gmail.com>
2023-07-02 01:44:17 +0530

    feat(s3): added bucket

--- lib/greenaura-core-backend-stack.ts
+++ lib/greenaura-core-backend-stack.ts
@@ -1,17 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { aws_s3 as s3 } from 'aws-cdk-lib';
 
 export class GreenauraCoreBackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-    //This is sample comment
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'GreenauraCoreBackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new s3.Bucket(this, 'CdkTestBucket', {
+      versioned: true
+    });
   }
 }
