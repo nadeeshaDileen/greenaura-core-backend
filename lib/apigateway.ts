7ac18470cae87b24827ff9078efd9f70e5d67387
nadeeshaDileen <nadeeshadileen@gmail.com>
2023-07-04 00:48:26 +0530

    fix(corelambda): fixed

--- lib/apigateway.ts
+++ lib/apigateway.ts
@@ -32,9 +32,7 @@ export class GreenauraCoreApiGateway extends Construct {
             exports.handler = async function(event, context) {
               return {
                 statusCode: 200,
-                body: ${JSON.stringify({
-                  message: 'Welcome to, Greenaura backend!',
-                })},
+                body: "Welcome to, Greenaura backend!",
                 headers: {
                   'Access-Control-Allow-Origin': '*',
                   'Access-Control-Allow-Credentials': true,
