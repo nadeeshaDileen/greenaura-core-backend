aa01797278646e18364988c537718a77123f154f
nadeeshaDileen <nadeeshadileen@gmail.com>
2023-07-03 23:41:22 +0530

    fix(corelambda): enable cors

--- lib/apigateway.ts
+++ lib/apigateway.ts
@@ -1,47 +1,51 @@
-import * as apigw from "aws-cdk-lib/aws-apigateway";
-import * as lambda from "aws-cdk-lib/aws-lambda";
-import { Construct } from "constructs";
+import * as apigw from 'aws-cdk-lib/aws-apigateway'
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+import { Construct } from 'constructs'
 
 interface GreenauraCoreApigwProps {
-  restApiName: string;
-  stageName: string;
+  restApiName: string
+  stageName: string
 }
 
 export class GreenauraCoreApiGateway extends Construct {
-  api;
+  api
 
   constructor(scope: Construct, id: string, props: GreenauraCoreApigwProps) {
-    super(scope, id);
+    super(scope, id)
 
     this.api = new apigw.RestApi(this, `greenaura-core`, {
-      description: "Greenaura Core API Gateway",
+      description: 'Greenaura Core API Gateway',
       restApiName: props.restApiName,
       deployOptions: {
         stageName: props.stageName,
       },
-    });
+    })
 
     //API Resources
-    const coreResource = this.api.root.addResource("core-endpoint");
+    const coreResource = this.api.root.addResource('core-endpoint')
 
     // Create the Lambda function
-    const coreLambdaFunction = new lambda.Function(this, "CoreLambdaFunction", {
+    const coreLambdaFunction = new lambda.Function(this, 'CoreLambdaFunction', {
       runtime: lambda.Runtime.NODEJS_14_X,
-      handler: "index.handler",
+      handler: 'index.handler',
       code: lambda.Code.fromInline(`
             exports.handler = async function(event, context) {
               return {
                 statusCode: 200,
-                body: 'Welcome to, Greenaura backend!'
+                body: 'Welcome to, Greenaura backend!',
+                headers: {
+                  'Access-Control-Allow-Origin': '*',
+                  'Access-Control-Allow-Credentials': true,
+              }
               };
             };
           `),
-    });
+    })
 
     // GET /core-endpoint
     coreResource.addMethod(
-      "GET",
+      'GET',
       new apigw.LambdaIntegration(coreLambdaFunction)
-    );
+    )
   }
 }
